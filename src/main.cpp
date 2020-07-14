56bf395914778431b3e24efc6a9d7b1c92cb407c
Pascal Pieper <Accounts@PascalPieper.de>
2020-07-14 11:27:50 +0200

    Communication working

--- src/main.cpp
+++ src/main.cpp
@@ -8,19 +8,22 @@
 
 
 #include <Arduino.h>
+#define USE_USBCON
 #include <ros.h>
 #include <rosserial_arduino/Adc.h>
 
 ros::NodeHandle nh;
 
 rosserial_arduino::Adc adc_msg;
-ros::Publisher p("head_mcu", &adc_msg);
+ros::Publisher p("adc", &adc_msg);
 
 void setup()
 {
   pinMode(13, OUTPUT);
-  memset(&adc_msg, 0, sizeof(rosserial_arduino::Adc));
+  pinMode(2, INPUT_PULLUP);
+  pinMode(3, INPUT_PULLUP);
 
+  nh.getHardware()->setBaud(115200);
   nh.initNode();
   nh.advertise(p);
 }
@@ -28,15 +31,18 @@ void setup()
 //We average the analog reading to elminate some of the noise
 int averageAnalog(int pin){
   int v=0;
-  for(int i=0; i<4; i++) v+= analogRead(pin);
-  return v/4;
+  for(int i=0; i<16; i++) v+= analogRead(pin);
+  return v/16;
 }
 
 void loop()
 {
-  adc_msg.adc0 = averageAnalog(0);
+  adc_msg.adc0 = averageAnalog(A0);
+  adc_msg.adc1 = digitalRead(2);
+  adc_msg.adc2 = digitalRead(3);
 
   p.publish(&adc_msg);
 
   nh.spinOnce();
+  delay(2);
 }
